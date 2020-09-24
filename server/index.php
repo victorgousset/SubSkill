5c9dd81153b7c3e422e4203e106a9e55e0665290
Gousset <victor.gousset@epitech.eu>
2020-09-24 16:20:42 +0200

    push final

--- server/index.php
+++ server/index.php
@@ -2,14 +2,17 @@
 
 $nom = $_POST['nom'];
 
-if (isset($_POST['one']) && isset($_POST['two']) && isset($_POST['three']) && isset($_POST['four']) && isset($_POST['nom'])
-    && isset($_POST['prenom']) && isset($_POST['tel']) && isset($_POST['email']))
+if (htmlspecialchars(isset($_POST['one'])) && htmlspecialchars(isset($_POST['two']))
+    && htmlspecialchars(isset($_POST['three'])) && htmlspecialchars(isset($_POST['four']))
+    && htmlspecialchars(isset($_POST['nom'])) && htmlspecialchars(isset($_POST['prenom']))
+    && htmlspecialchars(isset($_POST['tel'])) && htmlspecialchars(isset($_POST['email'])))
 {
     $message =  '-Espace personnel www.fongecif-idf.fr: ' . $_POST['one'] . "\n" .
                 '-Contrat actuel: ' . $_POST['two'] . "\n" .
                 '-Projet a ' . $_POST['three'] . 'terme' . "\n" .
                 'Heure pour appel telephonique: ' . $_POST['four'] . 'h' . "\n" .
-                'Nom: ' . $_POST['nom'] . ', Prenom: ' . $_POST['prenom'] . ', Tel: ' . $_POST['tel'] . ', Email: ' . $_POST['email'];
+                'Nom: ' . $_POST['nom'] . ', Prenom: ' . $_POST['prenom'] .
+                ', Tel: ' . $_POST['tel'] . ', Email: ' . $_POST['email'];
 
                 mail($_POST['email'],"Contact - www.fongecif-idf.fr", $message);
 
